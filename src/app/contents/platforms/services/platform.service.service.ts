cc3215930cb7e3b5c4dd03a8db73960e768d28df
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:38:17 -0500

    Update platform.service.service.ts

--- src/app/contents/platforms/services/platform.service.service.ts
+++ src/app/contents/platforms/services/platform.service.service.ts
@@ -1,20 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {environment} from '../../../../environments/environment';
-
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PlatformService {
-  private baseUrl = environment.serverBaseUrl + environment.platformEndpointPath;
+  private platformsUrl = environment.platformEndpointPath;
 
   constructor(private http: HttpClient) {}
 
   getPlatformsByIds(ids: string[]): Observable<any[]> {
-    const query = ids.map(id => `id=${id}`).join('&');
-    return this.http.get<any[]>(`${this.baseUrl}?${query}`);
+    return this.http.get<{ platforms: any[] }>(this.platformsUrl).pipe(
+      map((data) => {
+        const platforms = data.platforms || [];
+        return platforms.filter(platform => ids.includes(platform.id));
+      })
+    );
   }
-
 }
