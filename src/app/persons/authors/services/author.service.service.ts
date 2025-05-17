a0b52da88522ca38dce4a3af6b234bcf6b1b9581
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:57:35 -0500

    Update author.service.service.ts

--- src/app/persons/authors/services/author.service.service.ts
+++ src/app/persons/authors/services/author.service.service.ts
@@ -2,21 +2,27 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-
 import { map } from 'rxjs/operators';
-import {Author} from '../model/author.entity';
+import { Author } from '../model/author.entity';
+
+interface DbResponse {
+  authors: Author[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthorService {
-  private baseUrl = environment.serverBaseUrl + environment.authorEndpointPath;
+  private baseUrl = environment.authorEndpointPath;
 
   constructor(private http: HttpClient) {}
 
-  getAuthorById(id: string): Observable<Author> {
-    return this.http.get<any[]>(`${this.baseUrl}?id=${id}`).pipe(
-      map(authors => new Author(authors[0] || {}))
+  getAuthorById(id: string): Observable<Author | {}> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const author = data.authors.find(author => author.id === id);
+        return author ? new Author(author) : {};
+      })
     );
   }
 }
