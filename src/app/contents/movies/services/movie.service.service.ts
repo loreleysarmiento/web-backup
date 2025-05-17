0c20cdb0ceffdd2eddfb9a3123385017814c323b
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 12:59:32 -0500

    Update movie.service.service.ts

--- src/app/contents/movies/services/movie.service.service.ts
+++ src/app/contents/movies/services/movie.service.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {environment} from '../../../../environments/environment';
-import {map} from 'rxjs/operators';
-
+import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,12 +13,17 @@ export class MovieService {
   constructor(private http: HttpClient) {}
 
   getMovies(): Observable<any[]> {
-    return this.http.get<any[]>(this.baseUrl);
+    return this.http.get<any[]>(this.baseUrl).pipe(
+      map(data => data.movies || []) 
+    );
   }
 
   getMovieById(id: string): Observable<any> {
-    return this.http.get<any[]>(`${this.baseUrl}?id=${id}`).pipe(
-      map(movies => movies[0] || {})  
+    return this.http.get<any[]>(this.baseUrl).pipe(
+      map(data => {
+        const movies = data.movies || [];
+        return movies.find(movie => movie.id.toString() === id) || {};
+      })
     );
   }
 }
