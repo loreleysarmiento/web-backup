857424934ac6ff7f598de278070e9309579dd288
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:06:25 -0500

    Update movie.service.service.ts

--- src/app/contents/movies/services/movie.service.service.ts
+++ src/app/contents/movies/services/movie.service.service.ts
@@ -1,26 +1,34 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
-import { map } from 'rxjs/operators';
+import {environment} from '../../../../environments/environment';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MovieService {
-  private baseUrl = environment.serverBaseUrl + environment.movieEndpointPath;
+  private baseUrl = `${environment.serverBaseUrl}${environment.movieEndpointPath}`;
 
   constructor(private http: HttpClient) {}
 
   getMovies(): Observable<any[]> {
     return this.http.get<any[]>(this.baseUrl).pipe(
-      map(data => data.movies || []) 
+      map(data => {
+        // Verifica que "movies" exista en la respuesta
+        if (!data.movies || !Array.isArray(data.movies)) {
+          console.error("No se encontraron películas");
+          return [];
+        }
+        return data.movies;
+      })
     );
   }
 
   getMovieById(id: string): Observable<any> {
     return this.http.get<any[]>(this.baseUrl).pipe(
       map(data => {
+        // Busca la película por ID
         const movies = data.movies || [];
         return movies.find(movie => movie.id.toString() === id) || {};
       })
