f7c7d748354e9c086835cc09e644dc88867cc170
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 12:41:38 -0500

    Update user.service.ts

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -1,33 +1,32 @@
 import { Injectable } from '@angular/core';
-import {BaseService} from '../../shared/services/base.service';
-import {User} from '../model/user.entity';
-import {environment} from '../../../environments/environment';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
-
-const userResourceEndpoint=environment.userEndpointPath;
+import { BaseService } from '../../shared/services/base.service';
+import { User } from '../model/user.entity';
+import { environment } from '../../../environments/environment';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService extends BaseService<User>{
-
+export class UserService extends BaseService<User> {
   constructor() {
     super();
-    this.resourceEndpoint=userResourceEndpoint;
+    this.resourceEndpoint = environment.userEndpointPath;
   }
+
   registerUser(user: User): Observable<User> {
     return this.create(user);
   }
+
   getUserById(userId: string): Observable<User> {
-    const url = `${environment.serverBaseUrl}${environment.userEndpointPath}?id=${userId}`;
+    const url = `${environment.serverBaseUrl}${environment.userEndpointPath}/${userId}`;
     console.log("User URL:", url);
 
-    return this.http.get<User[]>(url).pipe(
-      map((users: User[]) => {
-        if (users && users.length > 0) {
-          console.log("User found:", users[0]);
-          return users[0];
+    return this.http.get<User>(url).pipe(
+      map((user: User) => {
+        if (user) {
+          console.log("User found:", user);
+          return user;
         }
         console.warn("No user found for ID:", userId);
         return {} as User;  // Retorna un usuario vacío si no se encuentra
