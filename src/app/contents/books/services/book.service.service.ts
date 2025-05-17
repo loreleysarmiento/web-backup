ad47483dd4022957df97c9dbd5c08ab30a858e25
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:37:11 -0500

    Update book.service.service.ts

--- src/app/contents/books/services/book.service.service.ts
+++ src/app/contents/books/services/book.service.service.ts
@@ -2,27 +2,41 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-
 import { map } from 'rxjs/operators';
-import {Book} from '../model/book.entity';
+import { Book } from '../model/book.entity';
+
+interface DbResponse {
+  books: Book[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
-  private baseUrl = environment.serverBaseUrl + environment.bookEndpointPath;
+  private booksUrl = environment.bookEndpointPath;
 
   constructor(private http: HttpClient) {}
 
   getBooks(): Observable<Book[]> {
-    return this.http.get<any[]>(this.baseUrl).pipe(
-      map(books => books.map(book => new Book(book)))
+    return this.http.get<DbResponse>(this.booksUrl).pipe(
+      map((data: DbResponse) => {
+        if (!data.books || !Array.isArray(data.books)) {
+          console.error("No se encontraron libros");
+          return [];
+        }
+        // Convierte los objetos en instancias de Book
+        return data.books.map(book => new Book(book));
+      })
     );
   }
 
-  getBookById(id: string): Observable<Book> {
-    return this.http.get<any[]>(`${this.baseUrl}?id=${id}`).pipe(
-      map(books => new Book(books[0] || {}))
+  getBookById(id: string): Observable<Book | {}> {
+    return this.http.get<DbResponse>(this.booksUrl).pipe(
+      map((data: DbResponse) => {
+        const books = data.books || [];
+        const foundBook = books.find(book => book.id === id);
+        return foundBook ? new Book(foundBook) : {};
+      })
     );
   }
 }
