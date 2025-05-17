c10f5f6cac612ee822189f360f1c563db1a3d4af
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:56:01 -0500

    Update actor.service.service.ts

--- src/app/persons/actors/services/actor.service.service.ts
+++ src/app/persons/actors/services/actor.service.service.ts
@@ -1,25 +1,35 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {environment} from '../../../../environments/environment';
+import { environment } from '../../../../environments/environment';
+import { map } from 'rxjs/operators';
 
+interface DbResponse {
+  actors: any[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ActorService {
-  private baseUrl = environment.serverBaseUrl + environment.actorEndpointPath;
+  private baseUrl = environment.actorEndpointPath;
 
   constructor(private http: HttpClient) {}
 
+  // Obtener todos los actores
   getActors(): Observable<any[]> {
-    return this.http.get<any[]>(this.baseUrl);
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => data.actors || [])
+    );
   }
 
+  // Obtener múltiples actores por sus IDs
   getActorsByIds(ids: string[]): Observable<any[]> {
-    const queryString = ids.map(id => `id=${id}`).join('&');
-    const url = `${this.baseUrl}?${queryString}`;
-    return this.http.get<any[]>(url);
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const actors = data.actors || [];
+        return actors.filter(actor => ids.includes(actor.id));
+      })
+    );
   }
-
 }
