738d894674978c83caaf145218bdab6976b18c1e
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:01:28 -0500

    Update auth.service.ts

--- src/app/users/pages/login-form/services/auth.service.ts
+++ src/app/users/pages/login-form/services/auth.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
-import {User} from '../../../model/user.entity';
+import { User } from '../../../model/user.entity';
 import { HttpClient } from '@angular/common/http';
-import {environment} from '../../../../../environments/environment';
-import { Observable, throwError } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { environment } from '../../../../../environments/environment';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -13,28 +13,28 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
- login(email: string, password: string): Observable<User | null> {
-  const url = `${environment.serverBaseUrl}${environment.userEndpointPath}`;
-  console.log("Login URL:", url);
+  login(email: string, password: string): Observable<User | null> {
+    const url = `${environment.serverBaseUrl}${environment.userEndpointPath}`;
+    console.log("Login URL:", url);
 
-  return this.http.get<User[]>(url).pipe(
-    map((users: User[]) => {
-      const foundUser = users.find(user => user.email === email && user.password === password);
-      if (foundUser) {
-        console.log("User authenticated:", foundUser);
-        return foundUser;
-      }
-      console.warn("Login failed for email:", email);
-      return null;
-    })
-  );
-}
-
+    return this.http.get<{ users: User[] }>(url).pipe(
+      map((data) => {
+        const foundUser = data.users.find(user => user.email === email && user.password === password);
+        if (foundUser) {
+          console.log("User authenticated:", foundUser);
+        
+          localStorage.setItem('currentUser', JSON.stringify(foundUser));
+          return foundUser;
+        }
+        console.warn("Login failed for email:", email);
+        return null;
+      })
+    );
+  }
 
   logout() {
     localStorage.removeItem('currentUser');
   }
-
 
   getCurrentUser(): User | null {
     const userJson = localStorage.getItem('currentUser');
