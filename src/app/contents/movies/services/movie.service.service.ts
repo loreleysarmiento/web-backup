9ce9db631109e12aab86b2b3a466361ff9c1cd55
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 14:13:56 -0500

    Update movie.service.service.ts

--- src/app/contents/movies/services/movie.service.service.ts
+++ src/app/contents/movies/services/movie.service.service.ts
@@ -1,42 +1,81 @@
+// src/app/reviews/services/review.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
-import { map } from 'rxjs/operators';
-import { Movie } from '../model/movie.entity';
+import { Observable, forkJoin, of } from 'rxjs';
+import { map, switchMap } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
+import { Review } from '../model/review.entity';
+import { Movie } from '../../contents/movies/model/movie.entity';
 
 interface DbResponse {
+  reviews: Review[];
   movies: Movie[];
 }
 
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService {
-  private moviesUrl = environment.movieEndpointPath;
+export class ReviewService {
+  private reviewsUrl = environment.reviewEndPointPath;
+
+  private hardcodedMovies: Movie[] = [
+    {
+      id: 'MV001',
+      titulo: 'Harry Potter and the Philosopher\'s Stone',
+      imagen: 'https://images.justwatch.com/poster/87608067/s718/harry-potter-and-the-philosophers-stone.jpg',
+      director_id: '',
+      actores_id: [],
+      genero: [],
+      duracion: 152,
+      fecha_lanzamiento: '2001-11-16',
+      idioma_original: 'Inglés',
+      pais_origen: 'Reino Unido',
+      plataformas_id: [],
+      sinopsis: ''
+    },
+    {
+      id: 'MV002',
+      titulo: 'The Godfather',
+      imagen: 'https://m.media-amazon.com/images/M/MV5BNGEwYjgwOGQtYjg5ZS00Njc1LTk2ZGEtM2QwZWQ2NjdhZTE5XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
+      director_id: '',
+      actores_id: [],
+      genero: [],
+      duracion: 175,
+      fecha_lanzamiento: '1972-03-24',
+      idioma_original: 'Inglés',
+      pais_origen: 'Estados Unidos',
+      plataformas_id: [],
+      sinopsis: ''
+    }
+  ];
 
   constructor(private http: HttpClient) {}
 
-  getMovies(): Observable<Movie[]> {
-    return this.http.get<DbResponse>(this.moviesUrl).pipe(
-      map((data: DbResponse) => {
-        if (!data.movies || !Array.isArray(data.movies)) {
-          console.error("No se encontraron películas");
-          return [];
-        }
-        // Convierte los objetos en instancias de Movie
-        return data.movies.map(movie => new Movie(movie));
+  getReviewsByUserId(userId: string): Observable<Review[]> {
+    return this.http.get<DbResponse>(this.reviewsUrl).pipe(
+      map((data) => data.reviews.filter(review => review.userId === userId) || [])
+    );
+  }
+
+  getEnrichedReviews(userId: string): Observable<any[]> {
+    return this.getReviewsByUserId(userId).pipe(
+      switchMap(reviews => {
+        const enrichedReviews = reviews.map((review) => {
+          const movie = this.hardcodedMovies.find(m => m.id === review.contenidoId) || {};
+          return {
+            ...review,
+            contenidoTitulo: movie.titulo || 'Desconocido',
+            contenidoImagen: movie.imagen || ''
+          };
+        });
+        return of(enrichedReviews);
       })
     );
   }
 
-  getMovieById(id: string): Observable<Movie | {}> {
-    return this.http.get<DbResponse>(this.moviesUrl).pipe(
-      map((data: DbResponse) => {
-        const movies = data.movies || [];
-        const foundMovie = movies.find(movie => movie.id === id);
-        return foundMovie ? new Movie(foundMovie) : {};
-      })
+  getReviewsByMovieId(contenidoId: string): Observable<Review[]> {
+    return this.http.get<DbResponse>(this.reviewsUrl).pipe(
+      map((data) => data.reviews.filter(review => review.contenidoId === contenidoId) || [])
     );
   }
 }
