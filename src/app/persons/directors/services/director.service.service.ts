7fce19d4ae88b1c563d8321d47142829ae9c7dec
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:59:25 -0500

    Update director.service.service.ts

--- src/app/persons/directors/services/director.service.service.ts
+++ src/app/persons/directors/services/director.service.service.ts
@@ -4,22 +4,32 @@ import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
 import { map } from 'rxjs/operators';
 
+interface DbResponse {
+  directors: any[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class DirectorService {
-  private baseUrl = environment.serverBaseUrl + environment.directorEndpointPath;
+  private baseUrl = environment.directorEndpointPath;
 
   constructor(private http: HttpClient) {}
 
+  // Obtener todos los directores
   getDirectors(): Observable<any[]> {
-    return this.http.get<any[]>(this.baseUrl);
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => data.directors || [])
+    );
   }
 
+  // Obtener un director por su ID
   getDirectorById(id: string): Observable<any> {
-    // Esto devuelve solo el primer director que coincida con el ID
-    return this.http.get<any[]>(`${this.baseUrl}?id=${id}`).pipe(
-      map(directors => directors[0] || {})
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const director = data.directors.find(director => director.id === id);
+        return director ? director : {};
+      })
     );
   }
 }
