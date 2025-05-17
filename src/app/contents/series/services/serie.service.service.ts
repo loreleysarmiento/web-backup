7b1db5f95a7c6f07db78e192ccff6ff679e31bb2
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:50:50 -0500

    Update serie.service.service.ts

--- src/app/contents/series/services/serie.service.service.ts
+++ src/app/contents/series/services/serie.service.service.ts
@@ -2,31 +2,45 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
-import {Serie} from '../model/serie.entity';
+import { Serie } from '../model/serie.entity';
+import { map } from 'rxjs/operators';
 
+interface DbResponse {
+  series: Serie[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class SerieService {
-  private baseUrl = environment.serverBaseUrl + environment.serieEndpointPath;
+  private baseUrl = environment.serieEndpointPath;
 
   constructor(private http: HttpClient) {}
 
   // Obtener todas las series
-  getSeries(): Observable<Serie[]> {
-    return this.http.get<Serie[]>(this.baseUrl);
+  getAllSeries(): Observable<Serie[]> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => data.series || [])
+    );
   }
 
   // Obtener una serie por su ID
-  getSerieById(id: string): Observable<Serie> {
-    return this.http.get<Serie>(`${this.baseUrl}/${id}`);
+  getSerieById(id: string): Observable<Serie | {}> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const serie = data.series.find(serie => serie.id === id);
+        return serie ? serie : {};
+      })
+    );
   }
 
   // Obtener múltiples series por sus IDs
   getSeriesByIds(ids: string[]): Observable<Serie[]> {
-    const queryString = ids.map(id => `id=${id}`).join('&');
-    const url = `${this.baseUrl}?${queryString}`;
-    return this.http.get<Serie[]>(url);
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const series = data.series || [];
+        return series.filter(serie => ids.includes(serie.id));
+      })
+    );
   }
 }
