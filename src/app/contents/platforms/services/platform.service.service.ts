c327479d60a7d0b576ee50184e1c2c901c1dcde7
loreleysarmiento <sarmientoloreley@gmail.com>
2025-05-17 13:46:56 -0500

    Update platform.service.service.ts

--- src/app/contents/platforms/services/platform.service.service.ts
+++ src/app/contents/platforms/services/platform.service.service.ts
@@ -5,19 +5,38 @@ import { environment } from '../../../../../environments/environment';
 import { Platform } from '../model/platform.entity';
 import { map } from 'rxjs/operators';
 
+interface DbResponse {
+  platforms: Platform[];
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class PlatformService {
-  private baseUrl = environment.serverBaseUrl + environment.platformEndpointPath;
+  private baseUrl = environment.platformEndpointPath;
 
   constructor(private http: HttpClient) {}
 
   getPlatformsByIds(ids: string[]): Observable<Platform[]> {
-    return this.http.get<{ platforms: Platform[] }>(this.baseUrl).pipe(
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
       map((data) => {
         const platforms = data.platforms || [];
         return platforms.filter(platform => ids.includes(platform.id));
+      })
+    );
+  }
+
+  getAllPlatforms(): Observable<Platform[]> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => data.platforms || [])
+    );
+  }
+
+  getPlatformById(id: string): Observable<Platform | {}> {
+    return this.http.get<DbResponse>(this.baseUrl).pipe(
+      map((data) => {
+        const platform = data.platforms.find(platform => platform.id === id);
+        return platform ? platform : {};
       })
     );
   }
